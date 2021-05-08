e9b0bc44d7ca157f022ffdb082ff95a4af7afa62
glglace <44548454+glglace@users.noreply.github.com>
2021-05-08 11:35:39 +0800

    Add files via upload

---
+++ tpet/6_way_generator/question_set.js
@@ -0,0 +1,8 @@
+var questionLines=function(){/*--這一行請勿更改--
+A群成員,A1,A2,A3,A4,A5
+B群成員,B1,B2,B3,B4,B5
+C群成員,C1,C2,C3,C4,C5
+A群成員,A1,A2,A3,A4,A5
+B群成員,B1,B2,B3,B4,B5
+C群成員,C1,C2,C3,C4,C5
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
