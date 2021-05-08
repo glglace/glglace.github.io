ddaafcab01060b86b94cfe0cf03efc207bd3cfa6
glglace <44548454+glglace@users.noreply.github.com>
2021-05-08 11:38:47 +0800

    Update question_set.js

--- tpet/6_way_generator/question_set.js
+++ tpet/6_way_generator/question_set.js
@@ -2,7 +2,7 @@ var questionLines=function(){/*--這一行請勿更改--
 A群成員,A1,A2,A3,A4,A5
 B群成員,B1,B2,B3,B4,B5
 C群成員,C1,C2,C3,C4,C5
-A群成員,A1,A2,A3,A4,A5
-B群成員,B1,B2,B3,B4,B5
-C群成員,C1,C2,C3,C4,C5
+顏色,黃,藍,紅,紫
+水果,香蕉,草莓,柳丁,葡萄
+季節,春,夏,秋,冬
 -----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
