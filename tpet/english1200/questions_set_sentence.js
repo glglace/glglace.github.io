7dd889be0a774195ff23fafcc472bf6f66076fe2
glglace <44548454+glglace@users.noreply.github.com>
2021-05-08 13:02:29 +0800

    Add files via upload

---
+++ tpet/english1200/questions_set_sentence.js
@@ -0,0 +1,58 @@
+﻿//每題有幾個選項
+numberOfOptions = 2;
+
+//每列最多可以有幾個選項
+numberOfOptionsPerRow = 1;	
+
+//每回合抽幾題題目
+numberOfQuestionsPerRound = 10;
+
+//出題時選擇題目的方式 true:亂數選題  false:按題庫順序
+select_questions_in_random = false; 
+
+//聲音檔路徑的前置網址
+//也就是會為 questionLines 的左欄前面自動加上的字串
+//
+// English 1200
+soundBaseURL = 'https://gsyan888.github.io/english1200/';
+
+//是否使用 TTS 的語音
+tts_enabled = true;
+
+//----------------
+//Google TTS 文字轉語音的設定
+//----------------
+tts_language = 'en';  // en : 英語,   zh_tw : 中文
+tts_speed = 0.3;  //語音的速度 0 ~ 1 (可用小數)
+tts_base_url = 'https://translate.google.com/translate_tts?ie=UTF-8&tl='+tts_language+'&client=tw-ob&ttsspeed='+tts_speed+'&q=';
+
+//-------------------------------
+//用來偵測換行字元用的測試區塊
+//請勿更動
+//並且放在題庫設定區塊之前
+//-------------------------------
+//測試區塊開始
+CR_LF_test = function(){/*--這一行請勿更改--
+CR_LF testing block
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
+CR_LF_First_Pos = CR_LF_test.indexOf('_LF')-'CR_LF'.indexOf('_LF');
+//測試區塊結束
+//-------------------------------
+
+
+//題庫
+//欄位分隔符號為兩個井字號(##)
+seperator = '##';
+
+//左欄為聲音檔路徑
+//右欄為中文
+questionLines = function(){/*--這一行請勿更改--
+Can he shake hands?##他會握手嗎？
+What a cute dog!##好可愛的狗！
+What can he do?##他會做什麼？
+What else can he do?##他還會做什麼？
+How smart!##多麼聰明呀！
+What a small world!##世界真小！
+He can do a lot of things.##他會做很多事情！
+I'm an English teacher in a junior high school.##我是一名國中的英語老師。
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2+(typeof(CR_LF_First_Pos)!='undefined' ? CR_LF_First_Pos:0),-9);
