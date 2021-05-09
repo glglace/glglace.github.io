b7240d4a46f195a7bb64eb3367badf16dd6314ae
glglace <44548454+glglace@users.noreply.github.com>
2021-05-09 13:36:23 +0800

    Update question_set.js

--- tpet/sentence-Dino6-Unit3-2/question_set.js
+++ tpet/sentence-Dino6-Unit3-2/question_set.js
@@ -1,8 +1,8 @@
-﻿var questionLines=function(){/*--這一行請勿更改--
+var questionLines=function(){/*--這一行請勿更改--
 什麼人,I,You,He,She,We,They,Danny,Owen,Irene
-有沒有,have,has,don't have, doesn't have
-學科,art,Chinese,English,math,music,PE,science,social studies
-課 在, class on
-星期幾,Monday.,Tuesday.,Wednesday.,Thursday.,Friday.
+助動詞,don't,doesn't
+動詞,have,has
+什麼課,art class,Chinese class,English class,math class,music class,PE class,science class,social studies class
+在星期幾,on Monday.,on Tuesday.,on Wednesday.,on Thursday.,on Friday.
 得分,5,4,3,2,1
 -----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
