f090c43f1c72a716d40fb6eb10cace5f58bacdec
glglace <44548454+glglace@users.noreply.github.com>
2021-05-09 11:39:06 +0800

    Add files via upload

---
+++ tpet/sentence-Dino6-Unit3-2/question_set.js
@@ -0,0 +1,8 @@
+﻿var questionLines=function(){/*--這一行請勿更改--
+什麼人,I,You,He,She,We,They,Danny,Owen,Irene
+有沒有,have,has,don't have, doesn't have
+學科,art,Chinese,English,math,music,PE,science,social studies
+課 在, class on
+星期幾,Monday.,Tuesday.,Wednesday.,Thursday.,Friday.
+得分,5,4,3,2,1
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
---
+++ tpet/sentence-Dino6-Unit3-3/question_set.js
@@ -0,0 +1,8 @@
+﻿var questionLines=function(){/*--這一行請勿更改--
+助動詞,Do,Does
+什麼人,I,you,he,she,we,they,Danny,Owen,Irene,Niki
+有,have,has
+什麼課,art class,Chinese class,English class,math class,music class,PE class,science class,social studies class
+在星期幾嗎?,on Monday?,on Tuesday?,on Wednesday?,on Thursday?,on Friday?
+得分,5,4,3,2,1
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
