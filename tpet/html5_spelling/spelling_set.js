4edc7872166b807a6b8e234462c09b61d4b90ba7
glglace <44548454+glglace@users.noreply.github.com>
2022-01-07 20:09:11 +0800

    Add files via upload

---
+++ tpet/html5_spelling/spelling_set.js
@@ -0,0 +1,9 @@
+var URIformatVars = function(){/*--這一行請勿更改--
+&title=單字高手&
+&number_enabled=no&
+&logger_url=&
+&datafolder=data&
+&menu=
+單字-命運航班第一季第10集,02.js
+&
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
