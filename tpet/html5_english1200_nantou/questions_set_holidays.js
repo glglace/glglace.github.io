1535780b8b445557136a15ffa2540f44541dcf63
glglace <44548454+glglace@users.noreply.github.com>
2022-03-01 20:38:23 +0800

    Update questions_set_holidays.js

--- tpet/html5_english1200_nantou/questions_set_holidays.js
+++ tpet/html5_english1200_nantou/questions_set_holidays.js
@@ -72,13 +72,21 @@ dragon boat race##划龍舟賽
 rice##米
 dumpling##水餃
 rice dumpling##粽子
+zongzi##粽子
 festival##節日
 Dragon Boat Festival##龍舟節，端午節
 carnations##康乃馨
+gift##禮物
+card##卡片
+make a card##做卡片
+sing a song##唱一首歌
 Mother's Day##母親節
 BBQ party##烤肉派對
 watch the moon##賞月
 cake##蛋糕
+birthday#生日
+birthday cake##生日蛋糕
+birthday card##生日卡片
 moon cake##月餅
 Jade Rabbit##玉兔
 pomelo##柚子
@@ -86,17 +94,32 @@ Moon Festival##月亮節，中秋節
 hunt##打獵
 Easter##復活節
 Easter bunny##復活節兔
-Easter egg hunt##福活節尋找彩蛋
+Easter egg##復活節彩蛋
+Easter egg hunt##復活節尋找彩蛋
 go egg hunting##去找彩蛋
 trick-or-treat##不給糖就搗蛋
+witch##巫婆
 Halloween##萬聖節前夕
+pumpkin##南瓜
+pie##派
+pumpkin pie##南瓜派
 turkey##火雞
 potatoes##馬鈴薯
+thanks##感謝
+giving##給予
 Thanksgiving##感恩節
 fire##火
 cracker##餅乾，炮竹
 firecracker##鞭炮
+spring couplets##春聯
+envelope##信封
+red envelope##紅包
+lucky money##壓歲錢
+rice cake##年糕
+lion dance##舞獅
+dragon dance##舞龍
 Chinese New Year##中國新年，春節
+During Chinese New Year, we always eat fish and wish for a peaceful year.##過年期間，我們總是吃魚和祈求和平的一年。
 eve##前夕，前一夜
 New Year's Eve##新年的前夕，除夕
 hot pot##火鍋
@@ -105,5 +128,19 @@ family##家庭，家人
 reindeer##麋鹿
 church##教堂
 Santa Claus##聖誕老人
+Christmas tree##聖誕樹
+decorate the Christmas tree##裝飾聖誕樹
 Christmas##聖誕節
+Christmas Eve##聖誕夜
+my favorite festival##我最愛的節日
+lantern##燈籠
+lantern festival##元宵節
+sweep##打掃
+ancestors##祖先
+tomb##墳墓
+sweep ancestors' tomb##掃墓
+Tomb Sweeping Day##掃墓節，清明節
+clean the beach for the Earth##為地球清淨海灘
+recycle##回收
+Earth Day##地球日
 -----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2+(typeof(CR_LF_First_Pos)!='undefined' ? CR_LF_First_Pos:0),-9);
