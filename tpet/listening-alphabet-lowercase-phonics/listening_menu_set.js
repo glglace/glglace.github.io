9694d69bc902d4f0257a69b04870c8aed3993f5b
glglace <44548454+glglace@users.noreply.github.com>
2022-11-08 14:39:02 +0800

    Add files via upload

---
+++ tpet/listening-alphabet-lowercase-phonics/listening_menu_set.js
@@ -0,0 +1,4 @@
+﻿menu_title='聽音辨字四選一';
+menu_items = new Array(
+  'Here We Go 1-1 字母拼讀,sample_listening_set.js'
+);
---
+++ tpet/listening-alphabet-lowercase-phonics/sample_listening_set.js
@@ -0,0 +1,27 @@
+﻿var URIformatVars = function(){/*--這一行請勿更改--
+&title=Here We Go 1-1 字母拼讀&
+&file_list=sample_images_list.js&
+&filename_escape=0&
+&hideTools=0&
+&autoHideSpeaker=1&
+&autoGotoNext=1&
+&debug=0&
+&options_total=4&
+&col=2&
+&imageWidth=360&
+&imageHeight=240&
+&fontsize=36&
+&fontname=Comic Sans MS&
+&label_fontsize=30&
+&label_fontname=Comic Sans MS&
+&show_text=no&
+&first_letter_change_color=yes&
+&questionsToAnswer=20&
+&scoreToAdd=5,4,3,2,1,0&
+&script_filename=list.bat&
+&file_list_load_delay=2&
+&images_folder=images&
+&mp3_folder=mp3&
+&logger=&
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
+
